bd7331502c72cf5c49e9327854df220ec20bdb54
AKASH ALAM <mesbahulalamakash2014@gmail.com>
2026-06-23 18:26:11 +0600

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int mx = 0, i = 0, j = height.size() - 1;
+        while(i < j){
+            mx = max(mx, min(height[i], height[j]) * (j - i));
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+        return mx;
+    }
+};
