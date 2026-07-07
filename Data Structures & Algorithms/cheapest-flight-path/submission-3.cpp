d34c9cb9e98583ade54c406cc85728c0802b671c
AKASH ALAM <mesbahulalamakash2014@gmail.com>
2026-07-07 14:52:35 +0600

    Add: cheapest-flight-path - submission-3

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-3.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector <pair<int, int>> g[105];
+        for(auto it : flights){
+            g[it[0]].push_back({it[1], it[2]});
+        }
+
+        queue<pair<pair<int, int>, int>> pq;
+        vector <int> dis(n+1, 1e8);
+        pq.push({{0, 0}, src});
+
+        while(!pq.empty()){
+            auto it = pq.front();
+            pq.pop();
+            int u = it.second;
+            int cnt = it.first.first;
+            int w = it.first.second;
+
+            for(auto [v, cost] : g[u]){
+                if(cnt <= k and w + cost < dis[v]){
+                    dis[v] = w + cost;
+                    pq.push({{cnt + 1, w + cost}, v});
+                }
+            }
+        }
+        if(dis[dst] != 1e8) return dis[dst];
+        return -1;
+    }
+};
